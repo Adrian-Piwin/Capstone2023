2c48130d9f734db2f2bdf9680953627c03f3980e
Adrian Piwin <adrian7048@gmail.com>
2023-10-10 00:18:07 -0400

    Created DB service

--- Assets/Scripts/Database/DBService.cs
+++ Assets/Scripts/Database/DBService.cs
@@ -4,35 +4,63 @@ using System.Data.SqlClient;
 
 public class DBService : MonoBehaviour
 {
-    private string connectionString = "Server=142.93.1.207;Database=CampusQuest;User Id=sa;Password=FraPre!A;";
+    private string connectionString;
+    private SqlConnection connection;
 
-    // Start is called before the first frame update
-    void Start()
+    // Constructor
+    public DBService()
     {
-        using (SqlConnection connection = new SqlConnection(connectionString))
+        // Create the connection string
+        connectionString = "Server=142.93.1.207;Database=CampusQuest;User Id=sa;Password=FraPre!A;"; ;
+
+        // Initialize and open the connection
+        connection = new SqlConnection(connectionString);
+        connection.Open();
+    }
+
+    // Close the database connection when the object is destroyed
+    private void OnDestroy()
+    {
+        if (connection != null && connection.State == ConnectionState.Open)
         {
-            connection.Open();
-
-            using (SqlCommand command = connection.CreateCommand())
-            {
-                command.CommandText = "SELECT * FROM players";
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        Debug.Log("id: " + reader[0].ToString());
-                        Debug.Log("val: " + reader[1].ToString());
-                    }
-                }
-            }
-
             connection.Close();
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Execute a SQL query and return the result as a DataTable
+    public DataTable ExecuteQuery(string query)
     {
+        DataTable dataTable = new DataTable();
+        using (SqlCommand command = new SqlCommand(query, connection))
+        {
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                adapter.Fill(dataTable);
+            }
+        }
+        return dataTable;
+    }
 
+    // Execute a non-query SQL command (e.g., INSERT, UPDATE, DELETE)
+    public int ExecuteNonQuery(string query)
+    {
+        using (SqlCommand command = new SqlCommand(query, connection))
+        {
+            return command.ExecuteNonQuery();
+        }
+    }
+
+    // Example: Select all players from a "players" table
+    public DataTable SelectAllPlayers()
+    {
+        string query = "SELECT * FROM players";
+        return ExecuteQuery(query);
+    }
+
+    // Example: Insert a new player into the "players" table
+    public int InsertPlayer(string playerName)
+    {
+        string query = $"INSERT INTO players (Name) VALUES ('{playerName}')";
+        return ExecuteNonQuery(query);
     }
 }
